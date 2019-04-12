d8609df97d0779cd22ace21f6b194452383a586e
hocgin <hocgin@gmail.com>
2019-04-12 15:02:38 +0800

    新增 spring boot + redis 缓存案例 - 管道

--- spring-boot-redis/src/test/java/in/hocg/redis/pipelined/RedisPipelinedTests.java
+++ spring-boot-redis/src/test/java/in/hocg/redis/pipelined/RedisPipelinedTests.java
@@ -1,9 +1,11 @@
 package in.hocg.redis.pipelined;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -21,5 +23,19 @@ public class RedisPipelinedTests {
     @Autowired
     private RedisTemplate redisTemplate;
     
-
+    
+    /**
+     * 测试管道
+     */
+    @Test
+    public void testPipelined() {
+        redisTemplate.executePipelined((RedisCallback<Object>) connection -> {
+            for (long i = 0; i < 100000L; i++) {
+                connection.set(String.format("pipelined: %d", i).getBytes(), "66".getBytes());
+            }
+            return null;
+        });
+        log.debug("{}", "OK");
+    }
+    
 }
