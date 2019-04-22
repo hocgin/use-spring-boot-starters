fe936929e58d7329cc2e82cbeb8f2e58b9d50d89
hocgin <hocgin@gmail.com>
2019-04-22 22:19:21 +0800

    新增 spring boot + jpa + druid 案例

---
+++ spring-boot-jpa/src/main/java/in/hocg/jpa/JpaApplication.java
@@ -0,0 +1,19 @@
+package in.hocg.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by hocgin on 2019/4/22.
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@SpringBootApplication
+public class JpaApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApplication.class, args);
+    }
+    
+}
