e28fd965846a5caeda89e400e7ad759f96b26341
hcjin <hocgin@gmail.com>
2019-07-19 14:06:06 +0800

    基础结构

---
+++ spring-boot-graphql/src/main/java/in/hocg/graphql/GraphQLApplication.java
@@ -0,0 +1,19 @@
+package in.hocg.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by hocgin on 2019/4/22.
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@SpringBootApplication
+public class GraphQLApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQLApplication.class, args);
+    }
+    
+}
