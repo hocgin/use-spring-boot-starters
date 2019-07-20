2c6f35ed8888c41038f136fde864924f6ad1bffa
hcjin <hocgin@gmail.com>
2019-07-20 16:25:52 +0800

    基础结构

---
+++ spring-boot-graphql/src/main/java/in/hocg/graphql/resolver/ExampleSubscriptionResolver.java
@@ -0,0 +1,15 @@
+package in.hocg.graphql.resolver;
+
+import com.coxautodev.graphql.tools.GraphQLSubscriptionResolver;
+import in.hocg.graphql.domain.Example;
+
+/**
+ * @author hocgin
+ * @date 2019/7/20
+ */
+public class ExampleSubscriptionResolver implements GraphQLSubscriptionResolver {
+    
+    Example sub() {
+    
+    }
+}
