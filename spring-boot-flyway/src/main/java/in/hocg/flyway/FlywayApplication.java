17a0d6d571658badf749def8e569a5a9396424f3
hocgin <hocgin@gmail.com>
2019-09-20 18:04:00 +0800

    flyway

---
+++ spring-boot-flyway/src/main/java/in/hocg/flyway/FlywayApplication.java
@@ -0,0 +1,19 @@
+package in.hocg.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by hocgin on 2019/4/22.
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@SpringBootApplication
+public class FlywayApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayApplication.class, args);
+    }
+    
+}
